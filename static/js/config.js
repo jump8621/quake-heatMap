5f41b5702cdd0c0aa222cc144dab7e46bae98a45
Juliann Pezzullo <jump8621@gmail.com>
2021-01-20 23:59:52 -0500

    config files

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoianVtcDg2MjEiLCJhIjoiY2tpcncwZWEwMXR1NTJzbGlhZWd6ZW5sMyJ9.h5QZTQCeenTCfs3TYK3l0w";
---
+++ static/js/config2.js
@@ -0,0 +1,2 @@
+// API key
+mapboxgl.accessToken = "pk.eyJ1IjoianVtcDg2MjEiLCJhIjoiY2tpcncwZWEwMXR1NTJzbGlhZWd6ZW5sMyJ9.h5QZTQCeenTCfs3TYK3l0w";
